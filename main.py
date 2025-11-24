29cb16cc06d6dae1a4f0a27e6a13fbcc18b3e90d
esobirjon2197 <esobirjon2197@gmail.com>
2025-11-24 15:23:55 +0500

    Add multiple examples for counting spaces in strings

---
+++ main.py
@@ -0,0 +1,126 @@
+# 1-m
+matn = " salom dunyo "
+print(f"matn: {matn}")
+
+soni = 0
+for i in range(len(matn)):
+    if matn[i] == ' ':
+        soni += 1
+
+print(f"Prbellar soni: {soni}")
+
+
+
+# 2-m
+matn = " uzbekiston respublikasi "
+print(f"matn: {matn}")
+
+soni = 0
+for i in range(len(matn)):
+    if matn[i] == ' ':
+        soni += 1
+
+print(f"Prbellar soni: {soni}")
+
+
+
+# 3-m
+matn = "123 456 789"
+print(f"matn: {matn}")
+
+soni = 0
+for i in range(len(matn)):
+    if matn[i] == ' ':
+        soni += 1
+
+print(f"Probellar sonni: {soni}")
+
+
+# 4-m
+matn = "            "
+print(f"matn: {matn}")
+
+soni = 0
+for i in range(len(matn)):
+    if matn[i] == " ":
+        soni += 1
+
+print(f"Probellar soni: {soni}")
+
+
+# 5-m
+matn = "      "
+print(f"matn: {matn}")
+
+soni = 0
+for i in range(len(matn)):
+    if matn[i] == " ":
+        soni += 1
+
+print(f"Probellar soni: {soni}")
+
+
+
+# 6-m
+matn = "salom dunyo alik"
+print(f"matn: {matn}")
+
+soni = 0
+for i in range(len(matn)):
+    if matn[i] == " ":
+        soni += 1
+
+print(f"Probellar soni: {soni}")
+
+
+# 7-m
+matn = "toshkentshahri"
+print(f"matn: {matn}")
+
+soni = 0
+for i in range(len(matn)):
+    if matn[i] == " ":
+        soni += 1
+
+print(f"Probellar soni: {soni}")
+
+
+# 8-m
+matn = " python dasturlash tili "
+print(f"matn: {matn}")
+
+soni = 0
+for i in range(len(matn)):
+    if matn[i] == " ":
+        soni += 1
+
+print(f"Probellar soni: {soni}")
+
+
+
+# 9-m
+matn = "salom dunyoalik? rahmat"
+print(f"matn: {matn}")
+
+soni = 0
+for i in range(len(matn)):
+    if matn[i] == " ":
+        soni += 1
+
+print(f"Probellar soni: {soni}")
+
+
+
+# 10-m
+matn = "a b c d e f g"
+print(f"matn: {matn}")
+
+soni = 0
+for i in range(len(matn)):
+    if matn[i] == " ":
+        soni += 1
+
+print(f"Probellar soni: {soni}")
+
+
+
